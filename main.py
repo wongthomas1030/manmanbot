37f495b1305f00d6b5139fec2cf0f388e518717c
wongthomas1030 <wongthomas1030@gmail.com>
2022-06-01 00:16:31 +0800

    Intent Added
    Added Intent to let bot can chat in certain channel

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
+intents=discord.Intents.default()
+intents.members=True
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!',intents=intents)
 
 @bot.event
 async def on_ready():
@@ -10,13 +12,14 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     channel=bot.get_channel(981222516490117210)
+    print("Someone Join")
     await channel.send(f'{member} join!')
 
 @bot.event
 async def on_member_remove(member):
     channel=bot.get_channel(981222638980583526)
-    await channel.send(f'{member} join!')
-
+    print("Someone Leave")
+    await channel.send(f'{member} leave!')
 
 bot.run('OTgxMTk1OTcxNzA0ODExNTcw.GgVy4B.AeSb9lvl9LerpBes1Ne05H20XW2JMoRjg89HhQ')
 
